dab6c62f0a42cdac78545399c4608b8b36f99cb1
imcoco <imcoco@gmail.com>
2022-05-30 19:29:47 -0400

    edit variables within render method

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const game = {
     this.secretNum = Math.floor(Math.random() * 
     (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
     do {
-      this.render()
       this.prevGuesses.push(this.getGuess())
+      this.render()
     } while 
     (this.prevGuesses[this.prevGuesses.length -1] !== this.secretNum)
   },
@@ -25,36 +25,14 @@ const game = {
   },
 
   render: function() {
-    if (this.secretNum === this.prevGuesses[this.prevGuesses.length - 1]) {
-      alert(`Congrats! You guessed the number in ${this.prevGuesses.length}`)
+    if (this.secretNum === this.prevGuesses[this.prevGuesses.length - 1]){
+      alert(`Congrats! You guessed the number in ${this.prevGuesses.length} tries!`)
       return
     } else if (this.secretNum > this.prevGuesses[this.prevGuesses.length - 1]){
-        alert(`Your guess is too low. Previous guesses: ${game.prevGuesses}`)
+        alert(`Your guess is too low. Previous guesses: ${this.prevGuesses}`)
     } else if (this.secretNum < this.prevGuesses[this.prevGuesses.length - 1]){
-      alert(`Your guess is too high. Previous guesses: ${game.prevGuesses}`)
+      alert(`Your guess is too high. Previous guesses: ${this.prevGuesses}`)
     }
   }
 }
 game.play()
-//add getGuess method to game that prompts player to enter a guess with a message formatted as: Enter a guess between [smallestNum] and [biggestNum]. Hint - use a template literal for the prompt message.
-
-//Ensure that the `getGuess` method returns a value that:
-// - Is a *number*, not a *string*.
-// - Is between `smallestNum` and `biggestNum`, inclusive.
-// - Hints:
-//     - This is a great use case for a `while` loop.
-//     - `parseInt` returns `NaN` if the string cannot be parsed into a number.
-
-
-//From within the `play` method, invoke the `getGuess` method from inside a loop, and add the new guess to the `prevGuesses` array.
-// - Hint: this is an excellent use for a while loop (or even a do...while loop!)
-
-// Add a `render` method to `game` that `play` will call after a guess has been made that alerts:
-//     - If the secret has been guessed: `Congrats! You guessed the number in [number of prevGuesses]!`
-//     - Otherwise: `Your guess is too [high|low] Previous guesses: x, x, x, x`
-//     - Hints:
-//         - `render` won’t be able to access any of `play`’s local variables, e.g., `guess`, so be sure pass `render` any arguments as needed (you may not have built your program to use any, that's ok if that's the case!)
-//         - Template literals not only have interpolation, but they also honor whitespace - including line breaks!
-//         - The list of previous guesses can be generated using the array `join` method.
-
-//The play method should end (return) when the guess matches secretNum.
